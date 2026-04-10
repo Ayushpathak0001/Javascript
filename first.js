da0c1ac3b4caa775e4d6016f8462b8765a2bfc1b
Ayushpathak0001 <ayushraj132007@gmail.com>
2026-04-10 23:24:22 +0530

    firstjava script

---
+++ first.js
@@ -0,0 +1,4 @@
+console.log("Ayush Raj");
+console.log("Hello,I am from Bihar");
+console.log("I am doing BCA from Shobhit University");
+console.log("I want to pursue a good career in tech industry")
