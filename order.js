f49c902c4d6abd7771b17d28afbb69843917e99d
Ayushpathak0001 <ayushraj132007@gmail.com>
2026-04-19 22:44:08 +0530

    orderstatus

---
+++ order.js
@@ -0,0 +1,13 @@
+let order="delivered"
+if(order==="orderplaced"){
+    console.log("Your order is placed")
+}
+else if(order==="inprocess"){
+    console.log("Your order is in making")
+}
+else if(order==="ready"){
+    console.log("Your order is ready for deliverey")
+}
+else if(order==="delivered"){
+    console.log("Your order is delivered \nThank you for order ")
+}
